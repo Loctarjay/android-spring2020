87e4492926702558e45b7a57de69b852eb965de0
Jon Eikholm <32537566+joneikholmkea@users.noreply.github.com>
2020-02-27 17:15:05 +0100

    today's code

---
+++ firebasehello/main/java/com/joneikholm/firebasehello/storage/MemoryStorage.java
@@ -0,0 +1,10 @@
+package com.joneikholm.firebasehello.storage;
+
+import com.joneikholm.firebasehello.model.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryStorage {
+    public static List<Note> notes = new ArrayList<>();
+}
